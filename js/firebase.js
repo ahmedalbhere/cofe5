ec89cf07365a04f631fe3bdb214cc409a9c4c6ab
ahmedalbhere <ahmed01552732042@gmail.com>
2025-07-01 20:13:40 +0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+// تهيئة Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyYOUR_API_KEY",
+    authDomain: "your-project.firebaseapp.com",
+    databaseURL: "https://your-project.firebaseio.com",
+    projectId: "your-project",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
